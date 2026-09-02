5d32ec91dfe69a49ae52097de46ea81e228f1f54
mak1143 <206786647+mak1143@users.noreply.github.com>
2026-09-02 23:32:21 +0000

    modified files && fixed the view='topic' to view='topics'

--- mypett/urls.py
+++ mypett/urls.py
@@ -6,4 +6,6 @@ app_name = "mypett"
 urlpatterns = [
     # Homepage
     path('',views.index,name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topics, name='topics'),
 ]

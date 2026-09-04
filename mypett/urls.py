d039fd6efa8576236d21f1bc42cc4f9d77cec2fe
mak1143 <206786647+mak1143@users.noreply.github.com>
2026-09-04 11:32:40 +0000

    added: django-bootstrap5,refactored my base.html,added: topic.html,refactored: block contend DTl, fixed: the context from singular to plural

--- mypett/urls.py
+++ mypett/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     # Homepage
     path('',views.index,name='index'),
     path('topics/', views.topics, name='topics'),
-    # path('topics/<int:topic_id>/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]

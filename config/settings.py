3a7ef6ee386b8377d648c42dcae005b9729cfcfb
mak1143 <206786647+mak1143@users.noreply.github.com>
2026-09-11 18:36:35 +0000

    commit changes

--- config/settings.py
+++ config/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%0*g_@t2h6xk9^okm&*a3gw=8b+-58%yr5m%5!27ab*uws@532"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TRUE
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
 # my settings 
 LOGIN_URL = 'users:login'
+
 
 
 # Application definition
--- mypett/views.py
+++ mypett/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .forms import EntryForm, TopicForm
 from .models import Entry, Topic
@@ -16,7 +17,7 @@ def index(request):
 @login_required
 def topics(request):
     """show all topics"""
-    topics = Topic.objects.order_by("date_added")
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
     context = {"topics": topics}
     return render(request, "mypett/topics.html", context)
 
@@ -24,6 +25,10 @@ def topics(request):
 def topic(request, topic_id):
     """show a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
+    # make sure the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404
+    
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "mypett/topic.html", context)
@@ -40,6 +45,9 @@ def new_topic(request):
 
         if form.is_valid():
             form.save()
+            new_topic = form.save(commit=False)
+            new_topic.ower = request.user
+            new_topic.save()
             return redirect("mypett:topics")
 
     # Display a blank or invalid form
@@ -50,6 +58,8 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+            raise Http404
 
     if request.method != "POST":
         # No data submitted; create a blank form
